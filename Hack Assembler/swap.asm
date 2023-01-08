45c8d1fafbc6c7ab6a38c95d5e741a1fe3fec70a
Tehreem Iqbal <tehreemiqbal825@gmail.com>
2023-01-09 03:16:20 +0500

    Assembler added

---
+++ Hack Assembler/swap.asm
@@ -0,0 +1,25 @@
+//Program: swap.asm
+//flips the values of RAM[0] and RAM[1]
+//temp = R1
+// R1 = R0
+//R0 = temp
+@R1
+D=M
+@temp
+
+M=D // temp = R1
+@R0
+(Label)
+D=M
+@R1
+
+M=D // R1 = R0
+@15
+D=M
+@Label
+@R0
+M=D // R0 = temp
+(END)
+
+@END
+0;JMP
